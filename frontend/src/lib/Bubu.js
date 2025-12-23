e5e905abc34860d179b75e5a0cd43c4c93878059
Antzgech <engidaworkanteneh@gmail.com>
2025-12-23 01:32:19 -0500

    Add Bubu.js with user data and properties

---
+++ frontend/src/lib/Bubu.js
@@ -0,0 +1,17 @@
+const Bubu = {
+  id: "2",
+  telegramId: 2,
+  username: "Bubu",
+  first_name: "Dagi",
+  photo_url: "",
+  points: 1000,
+  currentLevel: 6,
+  badges: [2],
+  completedTasks: [6],
+  invitedFriends: 0,
+  levelScores: { 1: 100, 2: 100, 3: 90, 4: 50, 5: 200, 6: 80 },
+  createdAt: new Date().toISOString(),
+  lastActive: new Date().toISOString(),
+};
+
+export default Bubu;
