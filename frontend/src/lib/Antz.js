ad3c89328d583472ee5a25b42a71a833800575b3
Antzgech <engidaworkanteneh@gmail.com>
2025-12-23 23:56:46 -0500

    Refactor Antz object by removing unused fields
    Removed unused properties from Antz object.

--- frontend/src/lib/Antz.js
+++ frontend/src/lib/Antz.js
@@ -1,17 +1,10 @@
 const Antz = {
   id: "1",
-  telegramId: 1,
   username: "Antz",
   first_name: "Antz",
-  photo_url: "",
   points: 0,
-  currentLevel: 1,
-  badges: [],
   completedTasks: [],
-  invitedFriends: 0,
-  levelScores: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 },
-  createdAt: new Date().toISOString(),
-  lastActive: new Date().toISOString(),
+  completedLevels: []
 };
 
 export default Antz;
