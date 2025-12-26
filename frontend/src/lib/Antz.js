17771b5bc54c300f8d0adcc62665fd2496d484d5
Antzgech <engidaworkanteneh@gmail.com>
2025-12-25 21:27:22 -0500

    Delete frontend directory

--- frontend/src/lib/Antz.js
+++
@@ -1,9 +0,0 @@
-// This file will be overwritten at runtime with Telegram user data
-const Antz = {
-  id: null,
-  username: null,
-  first_name: null,
-  photo_url: null
-};
-
-export default Antz;
